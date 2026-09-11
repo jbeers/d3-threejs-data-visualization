4a24fd8f041feb9743be7c49f13eb2901a5486f2
Jacob Beers <jbeers@ortussolutions.com>
2026-09-10 23:40:08 -0500

    more cleanup

---
+++ layouts/default.vue
@@ -0,0 +1,8 @@
+<template>
+  <div
+    class="slidev-layout default"
+    @pointerdown="$event.currentTarget.classList.contains('demo-slide') && $event.stopPropagation()"
+  >
+    <slot />
+  </div>
+</template>
